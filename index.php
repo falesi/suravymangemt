6e11cca6c5508d3879679169cbc9fb0c551cafd1
Alinuswe Mwandobo <alinuswemwandobo@gmail.com>
2020-12-04 15:29:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+  <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+</head>
+
+<body>
+<div class="build-wrap"></div>
+<script>
+jQuery($ => {
+  $('.build-wrap').formBuilder()
+})
+</script> 
+</body>
+
+</html>
